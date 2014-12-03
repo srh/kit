dc09a701db9761bb13978629fac0c1914e79f6c7
Sam Hughes <sam@samuelhughes.com>
2014-12-03 09:50:30 -0800

    Implemented ast_toplevel_destroy, left ast_def_destroy unimplemented.

--- ast.c
+++ ast.c
@@ -1,8 +1,38 @@
 #include "ast.h"
 
-void ast_toplevel_destroy(struct ast_toplevel *a) {
+#include "slice.h"
+
+void ast_ident_destroy(struct ast_ident *a) {
+  (void)a;
+  /* Do nothing. */
+}
+
+void ast_def_destroy(struct ast_def *a) {
   (void)a;
   /* TODO: Implement. */
+}
+
+void ast_module_destroy(struct ast_module *a) {
+  ast_ident_destroy(&a->name);
+  SLICE_FREE(a->toplevels, a->toplevels_count, ast_toplevel_destroy);
+}
+
+void ast_import_destroy(struct ast_import *a) {
+  ast_ident_destroy(&a->ident);
+}
+
+void ast_toplevel_destroy(struct ast_toplevel *a) {
+  switch (a->tag) {
+  case AST_TOPLEVEL_IMPORT:
+    ast_import_destroy(&a->u.import);
+    break;
+  case AST_TOPLEVEL_MODULE:
+    ast_module_destroy(&a->u.module);
+    break;
+  case AST_TOPLEVEL_DEF:
+    ast_def_destroy(&a->u.def);
+    break;
+  }
 }
 
 void ast_file_init(struct ast_file *a,
