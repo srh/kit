a20c8f1d70c4144318b7312172ad6d69813dba30
Sam Hughes <sam@samuelhughes.com>
2015-02-01 19:10:49 -0800

    Timeloop example.
    Kira takes 6.812 s versus 2.375 for a C debug build, or 0.250 for a C optimized build.

---
+++ examples/timeloop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+
+int32_t kira_runloop1(void);
+
+int main(void) {
+  clock_t c = clock();
+
+#if 1
+  size_t acc = kira_runloop1();
+#else
+  size_t acc = 0;
+  for (size_t i = 0; i < 1000000000; i++) {
+    acc += i;
+  }
+#endif
+
+  clock_t d = clock();
+
+  printf("%u %f\n", acc, (d - c) * (1.0 / CLOCKS_PER_SEC));
+  return 0;
+}
