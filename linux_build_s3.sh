68bd0d13a630fee6158d1b78af7effef57a7a1ec
Sam Hughes <sam@samuelhughes.com>
2015-10-11 02:35:13 -0700

    Added s3 build scripts.

---
+++ linux_build_s3.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cd src
+../s2 --linux main && gcc -m32 main.o dummy.c -o s3b
+./s3b --linux main && gcc -m32 main.o dummy.c -o s3
+mv s3 ../s3
+cd ..
+
---
+++ windows_build_s3.bat
@@ -0,0 +1,6 @@
+cd src
+..\s2 main && cl main.obj dummy.c /Fes3b /link /stack:0x100000,0x100000
+s3b main && cl main.obj dummy.c /Fes3
+del ..\s3.exe
+move s3.exe ..\s3.exe
+cd ..
