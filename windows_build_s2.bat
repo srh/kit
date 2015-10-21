00beca6159e7878a0e0d008392c467d70ca93a90
Sam Hughes <sam@samuelhughes.com>
2015-10-21 14:46:53 -0700

    Removed stack-size parameters in windows_build_s2/s3.
    They became unnecessary when we replaced s2 entirely with s3.

--- windows_build_s2.bat
+++ windows_build_s2.bat
@@ -1,5 +1,5 @@
 cd urc
 ..\out main && cl main.obj dummy.c /Fes2b
-s2b main && cl main.obj dummy.c /Fes2 /link /stack:0x100000,0x100000
+s2b main && cl main.obj dummy.c /Fes2
 move /y s2.exe ..\s2.exe
 cd ..
--- windows_build_s3.bat
+++ windows_build_s3.bat
@@ -1,5 +1,5 @@
 cd vrc
-..\s2 main && cl main.obj dummy.c /Fes3b /link /stack:0x100000,0x100000
+..\s2 main && cl main.obj dummy.c /Fes3b
 s3b main && cl main.obj dummy.c /Fes3
 move /y s3.exe ..\s3.exe
 cd ..
