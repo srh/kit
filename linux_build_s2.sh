94becb43d60bcdfecdd7047a7644134c4845f803
Sam Hughes <sam@samuelhughes.com>
2015-10-26 15:20:54 -0700

    Removed s2, removed urc.

--- linux_build_s2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd urc
-../out --linux main && gcc -m32 main.o dummy.c -o s2b
-./s2b --linux main && gcc -m32 main.o dummy.c -o s2
-mv s2 ../s2
-cd ..
-
--- windows_build_s2.bat
+++
@@ -1,5 +0,0 @@
-cd urc
-..\out main && cl main.obj dummy.c /Fes2b
-s2b main && cl main.obj dummy.c /Fes2
-move /y s2.exe ..\s2.exe
-cd ..
